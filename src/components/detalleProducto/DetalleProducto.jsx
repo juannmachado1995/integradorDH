d8da9c9aaa770e63e5e104fa34ce27fdd0b1e3f0
juannmachado1995 <juannmachado1995@gmail.com>
2024-02-21 22:40:48 -0300

    four

--- src/components/detalleProducto/DetalleProducto.jsx
+++ src/components/detalleProducto/DetalleProducto.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 
 const DetalleProducto = ({ srcImagen, nombreBici, onClose }) => {
 
+  
+
   return (
 
     <>
       <div className="detalle-producto-overlay">
         <article className="detalle-producto-card">
-          <button onClick={onClose} className='button'>Volver Atras</button>
-          <span>Detalles de la bici</span>
-          <h2>{nombreBici}</h2>
-          <img className='imagen-Detalle-Producto' src={srcImagen} alt="" />
-          <button className='button'>Ver mas fotos</button>
+          <div className='detalle-izquierda-card'>
+            <button onClick={onClose} className='button'>Volver Atras</button>
+            <span className='detalle-de-la-bici'>Detalles de la bici</span>
+            <text className='titulo-nombre-bici border-radius'>{nombreBici}</text>
+            <img className='imagen-Detalle-Producto' src={srcImagen} alt="" />
+            <button className='button'>Ver mas fotos</button>
+          </div>
+
+          <div>
+            <h2>Descripcion de producto</h2>
+            <p>Cuadro: Monoscocca Carbon <br/> Horquilla: Monoscocca Carbon</p>
+          </div>
+          
         </article>
       </div>
     </>
