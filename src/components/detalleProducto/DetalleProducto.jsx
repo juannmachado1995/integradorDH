495dd06212905f83776efb796b1df737e6efdb0c
juannmachado1995 <juannmachado1995@gmail.com>
2024-02-23 23:42:34 -0300

    twenty

--- src/components/detalleProducto/DetalleProducto.jsx
+++ src/components/detalleProducto/DetalleProducto.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DetalleProducto = ({ srcImagen, nombreBici, onClose }) => {
 
-  
+
 
   return (
 
@@ -11,17 +11,22 @@ const DetalleProducto = ({ srcImagen, nombreBici, onClose }) => {
         <article className="detalle-producto-card">
           <div className='detalle-izquierda-card'>
             <button onClick={onClose} className='button'>Volver Atras</button>
-            <span className='detalle-de-la-bici'>Detalles de la bici</span>
+            <span className='detalle-de-la-bici'>Detalles de la bicicleta</span>
             <text className='titulo-nombre-bici border-radius'>{nombreBici}</text>
             <img className='imagen-Detalle-Producto' src={srcImagen} alt="" />
             <button className='button'>Ver mas fotos</button>
           </div>
 
-          <div>
+          <div className='detalle-descripcion-producto'>
             <h2>Descripcion de producto</h2>
-            <p>Cuadro: Monoscocca Carbon <br/> Horquilla: Monoscocca Carbon</p>
+            <p>Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon
+              <br />  Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon
+              <br />  Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon
+              <br />  Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon</p>
+            <p>Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon</p>
+            <p>Cuadro: Monoscocca Carbon <br /> Horquilla: Monoscocca Carbon</p>
           </div>
-          
+
         </article>
       </div>
     </>
