e97305e2374dc80045f52b48f2b762c9703357e0
juannmachado1995 <juannmachado1995@gmail.com>
2024-02-29 02:11:31 -0300

    Agregar producto / listar producto

--- src/components/componentsCm/Form/FormProduct.jsx
+++ src/components/componentsCm/Form/FormProduct.jsx
@@ -1,60 +1,153 @@
 import React, { useState } from 'react';
 /*import ImageUploader from 'react-images-upload';*/
-import './formProduct.css'; 
-
+import './formProduct.css';
+import axios from "axios";
 
 const FormProduct = ({ onSubmit }) => {
-  const [product, setProduct] = useState({
-    id:'',
-    name: '',
-    description: '',
-    images: [],
-  });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setProduct({ ...product, [name]: value });
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [urlImagen, setUrlmagenes] = useState("");
+  const [tituloImagen, setTituloImagen] = useState("");
+  const [imagenes, setImagenes] = useState([]);
+
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
   };
 
-  const handleImageUpload = (images) => {
-    setProduct({ ...product, images });
+  const handleCategoriaChange = (event) => {
+    setCategoria(event.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(product); // Llama a la función onSubmit del componente padre
-    console.log('Producto enviado:', product);
+  const handleDescripcionChange = (event) => {
+    setDescripcion(event.target.value);
+  };
+
+  const handleUrlImagenChange = (event) => {
+    setUrlmagenes(event.target.value)
+  }
+  const handleTituloImagenChange = (event) => {
+    setTituloImagen(event.target.value)
+  }
+
+  const handleAgregarImagen = () => {
+    if (urlImagen !== "" && tituloImagen !== "") {
+      const nuevaImagen = { titulo: tituloImagen, urlImg: urlImagen };
+      setImagenes([...imagenes, nuevaImagen]);
+    } else {
+      alert("Por favor, ingrese tanto la URL como el título de la imagen.");
+    }
+  };
+
+  /*const handleImagenesChange = (event) => {
+    const urlImagen = Array.from(event.target.urlImagen);
+    setImagenes(urlImagen);
+  };*/
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    crearProducto();
+  };
+
+  const crearProducto = async () => {
+    const nuevoProducto = {
+      nombre: nombre,
+      descripcion: descripcion,
+      imagenes: imagenes,
+      /*
+      imagenes: imagenes.map((imagen) => ({
+        urlImagen: URL.createObjectURL(imagen),
+      })),*/
+      categoriaId: categoria,
+    };
+    console.log(nuevoProducto)
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/productos/registrar",
+        nuevoProducto
+      );
+      console.log("Producto guardado:", response.data);
+      const productoCreado = JSON.stringify(response.data)
+      alert("PRODUCTO CREADO CORRECTAMENTE" + productoCreado)
+
+      setNombre("");
+      setCategoria("");
+      setDescripcion("");
+      setImagenes([]);
+      setUrlmagenes(""); 
+      setTituloImagen("");
+
+    } catch (error) {
+      console.error("Error al guardar el producto:", error);
+      alert("ERROR AL CREAR PRODUCTO"+ error)
+    }
   };
 
   return (
 
     <div>
 
-    <form className="product-form" onSubmit={handleSubmit}>
-      
-      <h1>Agregar nuevos productos</h1>
-   
-      <div className="form-group"> 
-        <label>Nombre:</label>
-        <input
-          type="text"
-          name="name"
-          placeholder='ingrese nombre'
-          value={product.name}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Descripción:</label>
-        <textarea
-          name="description"
-          value={product.description}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="form-group">
+      <form className="product-form" onSubmit={handleSubmit}>
+
+        <h1>Agregar nuevos productos</h1>
+
+        <div className="form-group">
+          <label>Nombre:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder='ingrese nombre'
+            value={nombre}
+            onChange={handleNombreChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Descripcion:</label>
+          <input
+            type="text"
+            name="descripcion"
+            placeholder='Descripcion de producto'
+            value={descripcion}
+            onChange={handleDescripcionChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Titulo Imagen:</label>
+          <input
+            type="text"
+            name="Titulo Imagen"
+            placeholder='Titulo Imagen'
+            value={tituloImagen}
+            onChange={handleTituloImagenChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Url Imagen:</label>
+          <input
+            type="text"
+            name="Url imagen"
+            placeholder='Url imagen'
+            value={urlImagen}
+            onChange={handleUrlImagenChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Categoria:</label>
+          <input
+            type="text"
+            name="categoria"
+            placeholder='Categoria N°'
+            value={categoria}
+            onChange={handleCategoriaChange}
+          />
+        </div>
+
+
+
+        {/*  <div className="form-group">
         <label>Subir imágenes:</label>
-        {/*
         <ImageUploader
           withIcon={true}
            buttonText="Seleccionar imágenes"
@@ -62,16 +155,13 @@ const FormProduct = ({ onSubmit }) => {
           imgExtension={['.jpg', '.gif', '.png', '.gif', '.jpeg']}
           maxFileSize={5242880} // 5MB
         />
-  */}
-      </div>
-      <button type="submit">
-      <img src="./img/save.png" alt="Imagen del botón" className='saveButton' />
+      </div>*/}
 
-      Guardar Producto
-      </button>
-    </form>
-   
-    
+        <button type="submit" onClick={handleAgregarImagen}>
+          <img src="./img/save.png" alt="Imagen del botón" className='saveButton' />
+          Guardar Producto
+        </button>
+      </form>
 
     </div>
 
@@ -79,4 +169,3 @@ const FormProduct = ({ onSubmit }) => {
 };
 
 export default FormProduct;
-  
--- src/components/componentsCm/button/ProductoManejador.jsx
+++ src/components/componentsCm/button/ProductoManejador.jsx
@@ -1,72 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormProduct from '../Form/FormProduct';
-import ProductRegistration from '../Products/ProductRegistration' 
-import './button.css'; 
+import ProductRegistration from '../Products/ProductRegistration'
+import './button.css';
+import axios from 'axios';
 
 const ProductoManejador = () => {
   const [showAddForm, setShowAddForm] = useState(false);
-  const [products, setProducts] = useState([]);
   const [currentProduct, setCurrentProduct] = useState(null); // Nuevo estado para mantener el producto actual
-  
+  const [productos, setProductos] = useState([]);
+
   const handleAddClick = () => {
-   setShowAddForm(!showAddForm);
-   setCurrentProduct(null); // Reiniciar el producto actual al abrir el formulario
-   console.log('showAddForm después de handleListClick:', showAddForm);
+    setShowAddForm(!showAddForm);
+    setCurrentProduct(null); // Reiniciar el producto actual al abrir el formulario
+    console.log('showAddForm después de handleListClick:', showAddForm);
   };
 
-  const handleListClick = (newProduct) => {
-    setProducts([...products, newProduct]);
-    setShowAddForm(false); // Cierra el formulario después de agregar el producto
-    setCurrentProduct(newProduct); // Al agregar un producto, establecerlo como el producto actual
-    console.log('Mostrar lista de productos');
+  const handleListClick = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/productos/listar');
+      console.log('Productos obtenidos:', response.data);
+      setProductos(response.data);
+    } catch (error) {
+      console.error('Error al obtener productos:', error);
+    }
   };
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/productos/listar');
+        console.log('Productos obtenidos:', response.data);
+        setProductos(response.data);
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchProductos();
+  }, []); // Se ejecuta solo una vez al montar el componente
+
+  ;
 
   return (
     <div>
-    
+
       <div className="container-admin">
-        <button 
-        onClick={handleAddClick}>Agregar Producto
-        <img src="./img/imgBoton.png" alt="Imagen del botón" className='iconButton' />
+        <button
+          onClick={handleAddClick}>Agregar Producto
+          <img src="./img/imgBoton.png" alt="Imagen del botón" className='iconButton' />
         </button>
-        <button  onClick={() => setShowAddForm(false)}>Lista de Productos
-        
-        <img src="./img/imgBoton2.png" alt="Imagen del botón" className='iconButton' />
-        
+        <button onClick={() => { setShowAddForm(false); handleListClick(); }}>
+          Lista de Productos
+          <img src="./img/imgBoton2.png" alt="Imagen del botón" className='iconButton' />
         </button>
       </div>
-      {showAddForm && <FormProduct onSubmit={handleListClick} currentProduct={currentProduct}/>}
+      {showAddForm && <FormProduct onSubmit={handleListClick} currentProduct={currentProduct} />}
 
-      {products.length > 0 && !showAddForm && (
-        <div  className="product-list">
-          <h2>Lista de Productos:</h2>
-          <table className='table'>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Descripcion</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map((product, index) => (
-          <tr key={index}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td>{product.description}</td>
-            <td>
-                    <img
-                      src="./img/delete.png"
-                      alt="Eliminar"
-                      className="delete-icon"
-                      
-                    />
-                  </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      {productos.length > 0 && !showAddForm && (
+        <div>
+          <h1>Listado de Productos</h1>
+          <ul>
+            {productos.map(producto => (
+              <li key={producto.id}>
+                <h2>NOMBRE PRODUCTO: {producto.nombre}</h2>
+                <p>DESCRIPCION: {producto.descripcion}</p>
+                <p>ID PRODUCTO: {producto.id}</p>
+                <h3>IMÁGENES:</h3>
+                <ul>
+                  {producto.imagenes.map(imagen => (
+                    <li key={imagen.id}>
+                      <img src={imagen.urlImg} alt={imagen.titulo} />
+                      <p>{imagen.titulo}</p>
+                    </li>
+
+                  ))}
+                </ul>
+                <h1>---------------------------</h1>
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
